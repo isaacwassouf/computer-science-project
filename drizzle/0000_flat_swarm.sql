59c3178642c6ed1bdd41175b760cafb9904d337c
Isaac Wassouf <isaacwassouf@gmail.com>
2024-03-05 21:27:32 +0100

    fix: adjust migrations

--- drizzle/0000_flat_swarm.sql
+++ drizzle/0000_flat_swarm.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `users` (
 	`id` bigint AUTO_INCREMENT NOT NULL,
-	`full_name` varchar(256),
-	`email` varchar(256),
+	`name` varchar(256) NOT NULL,
+	`email` varchar(256) NOT NULL,
 	`image_url` text,
 	CONSTRAINT `users_id` PRIMARY KEY(`id`),
 	CONSTRAINT `users_email_unique` UNIQUE(`email`)
--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -2,8 +2,7 @@ import { mysqlTable, bigint, varchar, text } from 'drizzle-orm/mysql-core';
 
 export const users = mysqlTable('users', {
 	id: bigint('id', { mode: 'number' }).primaryKey().autoincrement(),
-	fullName: varchar('full_name', { length: 256 }),
-	email: varchar('email', { length: 256 }).unique(),
-	image_url: text('image_url'),
-	created_at: bigint('created_at', { mode: 'number' }).notNull()
+	name: varchar('name', { length: 256 }).notNull(),
+	email: varchar('email', { length: 256 }).unique().notNull(),
+	image_url: text('image_url')
 });
